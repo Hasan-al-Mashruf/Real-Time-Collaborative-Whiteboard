f201d4c34654fcaa069ac21ca44341307ea7927f
Hasan-al-Mashruf <mashruf125@gmail.com>
2025-03-09 10:26:12 +0600

    Initialise frontend with React and design with Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline text-red-400">Hlw World</h2>
+    </>
+  );
+}
+
+export default App;
